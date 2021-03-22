92077db138f186d83f2a59e3e1ab0214942286c6
Caitlyn <caitlynmiley.cm@gmail.com>
2021-03-21 20:26:27 -0500

    added buttons and a title to the new html page

--- script.js
+++ script.js
@@ -3,10 +3,12 @@
 // create a start button 
 
 // add click event listener to start button so the timer sets 
+
 // make a timer that decrements when answer === false 
+
 // when answer === true go to another question 
 // when no more questions the game ends 
 // when the game ends ask for initials
-// add initials and score (score+ timer) to localstorage 
+// add initials and score (score= timer) to localstorage 
 // sort leaderboard by highest score (store into an object)
 
