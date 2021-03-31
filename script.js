113b3cf8265d1348e4718074ad8d03a2fd40bdb0
Caitlyn <caitlynmiley.cm@gmail.com>
2021-03-31 18:49:16 -0500

    added a conditional that looks for the correct answer in the array is clicked. if it is clicked then it will move to the next question

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+// variables used to start the quiz
 var startButton = document.querySelector("#start");
 startButton.addEventListener("click", startQuiz);
 
@@ -7,16 +7,19 @@ var interval = 0;
 var secondsLeft = 60
 var currentTime = document.querySelector("#time");
 
-// question variables
-var questionArr = [q1, q2, q3, q4, q5];
+// question array variables for conditional
+
+var questionArrayay = [q1, q2, q3, q4, q5];
 var index = 0
-// answer variables
+
+// answer variables use these for contitional!! 
 var question = document.querySelector("#questions");
 var choice1El = document.querySelector("#A");
 var choice2El = document.querySelector("#B");
 var choice3El = document.querySelector("#C")
 var choice4El = document.querySelector("#D");
 
+// hide from html 
 choice1El.style.display = "none";
 choice2El.style.display = "none";
 choice3El.style.display = "none";
@@ -71,5 +74,64 @@ var q5 = {
 }
 
 
-var questionArr = [q1, q2, q3, q4, q5];
-var index = 0
+
+
+function render() {
+    //allows you to show question and choices (they were originally set to hidden before this function is ran)
+    question.style.display = "block";
+    choice1El.style.display = "table-row";
+    choice2El.style.display = "table-row";
+    choice3El.style.display = "table-row";
+    choice4El.style.display = "table-row";
+
+    //check if index is < questionArray & turns to text
+    if (index < questionArrayay.length) {
+        question.textContent = questionArray[index].question;
+        choice1El.textContent = questionArray[index].choice1;
+        choice2El.textContent = questionArray[index].choice2;
+        choice3El.textContent = questionArray[index].choice3;
+        choice4El.textContent = questionArray[index].choice4;
+        answer = questionArray[index].answer
+
+    // checks for answer & if it is what is clicked == answer then it will move to next question
+
+
+        if (secondsLeft > 10) {
+            choice1El.onclick = function () {
+                if (questionArray[index].choice1 == answer) {
+                    
+                    index++
+                    render()
+                } 
+            }
+
+            choice2El.onclick = function () {
+                if (questionArray[index].choice2 == answer) {
+                   
+                    index++
+                    render()
+                } 
+            }
+
+            choice3El.onclick = function () {
+                if (questionArray[index].choice3 == answer) {
+                    
+                    index++
+                    render()
+                }
+            }
+
+            choice4El.onclick = function () {
+                if (questionArray[index].choice4 == answer) {
+                    
+                    index++
+                    render()
+               
+            }
+        } 
+    } else {
+       console.log('you made it through the quiz!')
+    
+       
+    }
+}
